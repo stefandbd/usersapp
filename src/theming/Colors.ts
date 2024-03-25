76760152976125f1d5a201c2e68028513d73f371
Stefan <dbd.stefan@yahoo.com>
2024-03-25 19:43:57 +0200

    theming section

---
+++ src/theming/Colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  white: '#FFF',
+  whiteish: '#f4f2fb',
+  primary: 'green',
+  wave: '#EAF2DB',
+  darkGreen: '#5A5D54',
+  red: 'red',
+};
---
+++ src/theming/Images.ts
@@ -0,0 +1,7 @@
+const Images = {
+  wave: require('../assets/images/wave.svg'),
+  waveBg: require('../assets/images/wavebg.svg'),
+  waveBgg: require('../assets/images/wavebg.png'),
+};
+
+export default Images;
